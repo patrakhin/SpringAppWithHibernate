43985377f582203678e94f793fafb6f02d8b48d3
patrakhin <patrakhin@mail.ru>
2023-07-16 17:56:05 +0300

    add fix for full CRUD app

--- src/main/java/ru/patrakhin/models/Person.java
+++ src/main/java/ru/patrakhin/models/Person.java
@@ -21,13 +21,19 @@ public class Person {
     @Column(name = "age")
     private int age;
 
+    @Column(name = "email")
+    @NotEmpty(message = "Email should not be empty")
+    @Email
+    private String email;
+
     public Person() {
 
     }
 
-    public Person(String name, int age) {
+    public Person(String name, int age, String email) {
         this.name = name;
         this.age = age;
+        this.email = email;
     }
 
     public int getId() {
@@ -52,6 +58,14 @@ public class Person {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     @Override
